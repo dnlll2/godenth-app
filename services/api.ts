0a084a93119d5a6abd540ad563403e889fa0ff07
dnlll2 <dnlll2010@gmail.com>
2026-05-07 01:26:12 +0000

    feat: conectar app com API em producao

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = 'https://godenth-api-production.up.railway.app'
 
 const api = axios.create({
   baseURL: API_URL,
